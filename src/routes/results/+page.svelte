5b221296ed1ad74a8813a0be83c8767437399058
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2022-12-12 19:10:36 -0500

    results pages

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -1,7 +1,15 @@
 <script>
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
 	const query = $page.url.searchParams.get('search');
+
+	let photos = [];
+
+	onMount(async () => {
+		const res = await fetch(`/tutorial/api/album`);
+		photos = await res.json();
+	});
 </script>
 
 <div>hit {query}</div>
