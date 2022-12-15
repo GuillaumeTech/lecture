c16efe075097dbd80b3ebdea41736ec62fb6d782
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2022-12-14 19:32:57 -0500

    recipe compoenent

---
+++ src/routes/results/recipe.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+import Recipe from './recipe.svelte';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/svelte/writing-stories/introduction
+const meta = {
+  title: 'Recipe',
+  component: Recipe,
+} satisfies Meta<Recipe>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/svelte/writing-stories/args
+export const Primary: Story = {
+  args: {
+    title: 'mirepoix',
+    ingredients: ['carottes', 'oignon', 'celeri'],
+    link:'https://www.marmiton.org/recettes/recette_mirepoix-de-legumes-oublies_64353.aspx',
+    imageLink:'https://assets.afcdn.com/recipe/20150831/3146_w640h486c1cx1424cy2105.webp',
+    prepTime:'50',
+    cookTime:'40',
+    restTime:'30'
+},
+};
---
+++ src/routes/results/recipe.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	export let ingredients: Array<string> = [];
+	export let title: string;
+	export let link: string;
+	export let imageLink: string = '';
+	export let prepTime: string = '';
+	export let cookTime: string = '';
+	export let restTime: string = '';
+
+	const total = parseInt(cookTime) + parseInt(restTime) + parseInt(cookTime);
+</script>
+
+<div class="recipe">
+	<img height="150" src={imageLink} />
+	<div class="recipe-info">
+		<a href={link}>{title}</a>
+		<ul class="inline-list timings">
+			<li>Préparation: {prepTime} min,</li>
+			<li>Cuisson: {cookTime} min,</li>
+			<li>Repos: {restTime} min</li>
+		</ul>
+		<p>Total: {total} minutes</p>
+		<ul class="inline-list ingredients">
+			{#each ingredients as ingredient}
+				<li class="ingredient">{ingredient}</li>
+			{/each}
+		</ul>
+	</div>
+</div>
+
+<style lang="less">
+	.recipe {
+		display: flex;
+		flex-flow: row nowrap;
+	}
+	.recipe-info {
+		margin-left: 1rem;
+		a {
+			font-size: 1.5rem;
+		}
+		.inline-list {
+			padding-inline: 0;
+			&.timings {
+				li {
+					margin-right: 0.3rem;
+				}
+			}
+			li {
+				&.ingredient {
+					background-color: paleturquoise;
+					margin-right: 0.3rem;
+					padding: 0.1rem 0.3rem 0.1rem 0.3rem;
+				}
+				display: inline;
+			}
+		}
+	}
+</style>
